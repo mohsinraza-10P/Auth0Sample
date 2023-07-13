7f33fa7c710413116775902687096c8f4867e14d
Mohsin Raza <mohsin.raza@10pearls.com>
2023-07-13 16:09:43 +0500

    Initial Commit

---
+++ Auth0Sample/Auth0Sample/Constants.cs
@@ -0,0 +1,23 @@
+﻿namespace Auth0Sample
+{
+	public static class Constants
+    {
+        public const string Https = "https://";
+
+        public const string AppPackageName = "com.tenpearls.auth0sample";
+
+        public const string Audience = Https + "api.dev.theyield.com";
+
+        public const string Auth0Domain = "theyield-dev-sit.au.auth0.com";
+
+        public const string Auth0ClientId = "XsalWiCKTyKMNTppn7aiLwUogKrL2Lp4";
+
+        public const string DriodCallbackUrl = AppPackageName + "://" + Auth0Domain + "/android/" + AppPackageName + "/callback";
+
+        public const string iOSCallbackUrl = AppPackageName + "://" + Auth0Domain + "/ios/" + AppPackageName + "/callback";
+
+        public const string DroidIntentPath = "/android/" + AppPackageName + "/callback";
+
+    }
+}
+
---
+++ Auth0Sample/Auth0Sample/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Auth0Sample
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
+
