a84851d5eb207cbb4db2b686d49aae0acace1452
Mohsin Raza <mohsin.raza@10pearls.com>
2023-07-14 18:11:11 +0500

    Unused constants removed

--- Auth0Sample/Auth0Sample/Common/Constants.cs
+++ Auth0Sample/Auth0Sample/Common/Constants.cs
@@ -10,10 +10,6 @@
 
         public const string Auth0ClientId = "XsalWiCKTyKMNTppn7aiLwUogKrL2Lp4";
 
-        public const string DriodCallbackUrl = AppPackageName + "://" + Auth0Domain + "/android/" + AppPackageName + "/callback";
-
-        public const string iOSCallbackUrl = AppPackageName + "://" + Auth0Domain + "/ios/" + AppPackageName + "/callback";
-
         public const string DroidIntentPath = "/android/" + AppPackageName + "/callback";
 
         public const string Scope = "openid profile offline_access";
